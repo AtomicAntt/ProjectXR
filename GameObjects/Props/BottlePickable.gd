ebfa107d676965359d3d035f15a52821a9ba4f3f
AtomicAntt <anthonychi36@gmail.com>
2026-01-16 17:04:55 -0800

    Bottle opens with force and refreshes

--- GameObjects/Props/BottlePickable.gd
+++ GameObjects/Props/BottlePickable.gd
@@ -2,9 +2,13 @@
 extends XRToolsPickable
 
 @export var bottle_cap: PackedScene
+@export var open_force: float = 3.0
 
 @onready var cap_mesh: MeshInstance3D = $Cap
 @onready var open_sound: AudioStreamPlayer3D = $BottleOpen
+
+func refresh() -> void:
+	cap_mesh.visible = true
 
 func action() -> void:
 	super.action()
@@ -18,6 +22,6 @@ func action() -> void:
 		cap_instance.set_as_top_level(true)
 		add_child(cap_instance)
 		cap_instance.global_transform = cap_mesh.global_transform
-		cap_instance.apply_impulse(cap_mesh.global_transform.basis.y, cap_mesh.global_position - cap_mesh.global_transform.basis.y)
+		cap_instance.apply_impulse(cap_mesh.global_transform.basis.y * open_force, cap_mesh.global_position - cap_mesh.global_transform.basis.y)
 		cap_mesh.visible = false
 		open_sound.play()
--- Levels/ItemRefresher.gd
+++ Levels/ItemRefresher.gd
@@ -1,16 +1,18 @@
 extends StaticBody3D
 
-@export var pickables: Array[XRToolsPickable]
+#@export var pickables: Array[XRToolsPickable]
 
 var pickable_position_data: Dictionary = {} # XRToolsPickable : transform
 
 func _ready() -> void:
-	for item: XRToolsPickable in pickables:
+	for item: XRToolsPickable in get_tree().get_nodes_in_group("Refreshable"):
 		pickable_position_data[item] = item.global_transform
 
 func _on_timer_timeout() -> void:
-	for item: XRToolsPickable in pickables:
+	for item: XRToolsPickable in get_tree().get_nodes_in_group("Refreshable"):
 		if item.global_position.y < global_position.y:
 			item.global_transform = pickable_position_data[item]
 			item.linear_velocity = Vector3.ZERO
 			item.angular_velocity = Vector3.ZERO
+			if item.has_method("refresh"):
+				item.refresh()
