8bbdad734287bad361bdd675f326bd04bb4e14b3
AtomicAntt <anthonychi36@gmail.com>
2026-01-18 12:36:16 -0800

    Add pouring/spilling logic to bottles

--- GameObjects/Props/BottlePickable.gd
+++ GameObjects/Props/BottlePickable.gd
@@ -4,8 +4,14 @@ extends XRToolsPickable
 @export var bottle_cap: PackedScene
 @export var open_force: float = 3.0
 
+## The largest angle between gravity and the object's orientation before spilling.
+@export_range(0, 180) var angle_threshold: float = 90
+
 @onready var cap_mesh: MeshInstance3D = $Cap
 @onready var open_sound: AudioStreamPlayer3D = $BottleOpen
+
+@onready var gravity_direction: Vector3 = Vector3.DOWN
+@onready var threshold: float = cos(deg_to_rad(angle_threshold))
 
 func refresh() -> void:
 	cap_mesh.visible = true
@@ -25,3 +31,27 @@ func action() -> void:
 		cap_instance.apply_impulse(cap_mesh.global_transform.basis.y * open_force, cap_mesh.global_position - cap_mesh.global_transform.basis.y)
 		cap_mesh.visible = false
 		open_sound.play()
+
+var within_threshold: bool = false
+
+func _physics_process(_delta: float) -> void:
+	# returns the cos(theta) between the gravity vector and the object's local up vector
+	# -1 represents that the object is upright (since the gravity vector should be 180 degrees from object)
+	# 1 represents the object up direction and gravity direction are the same
+	var angle_between: float = gravity_direction.dot(global_transform.basis.y)
+	
+	# make it more difficult to exit the pouring/not pouring state
+	if within_threshold:
+		angle_between -= 0.03
+	else:
+		angle_between += 0.03
+	
+	if angle_between <= threshold or cap_mesh.visible:
+		# no spill
+		within_threshold = true
+		print("no spill")
+	else: 
+		# spill
+		within_threshold = false
+		print("spill")
+	
