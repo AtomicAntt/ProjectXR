ffb19152c6bf836e7f442bb6e685748793195daf
AtomicAntt <anthonychi36@gmail.com>
2026-02-13 11:49:29 -0800

    move dialogue code to be inherting the 2d to 3d viewport script

--- UI/dialogue.gd
+++ UI/dialogue.gd
@@ -1,11 +1,8 @@
-extends Node
+@tool
+extends XRToolsViewport2DIn3D
 
-var xr_camera: XRCamera3D
-var dialogue_node: Node3D
-
-func _ready() -> void:
-	xr_camera = get_tree().get_nodes_in_group("XRCamera3D")[0]
-	dialogue_node = get_parent()
+@onready var xr_camera: XRCamera3D = get_tree().get_nodes_in_group("XRCamera3D")[0]
 
 func _physics_process(_delta: float) -> void:
-	dialogue_node.look_at(xr_camera.global_position, Vector3.UP, true)
+	if not Engine.is_editor_hint():
+		look_at(xr_camera.global_position, Vector3.UP, true)
