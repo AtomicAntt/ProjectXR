7ad4606e3688844474c0033b1ca74a5e432f5b82
AtomicAntt <anthonychi36@gmail.com>
2026-02-15 23:20:31 -0800

    Haptic feedback is given when the pointer is hitting UI buttons only.

--- Player/haptics_manager.gd
+++ Player/haptics_manager.gd
@@ -19,8 +19,10 @@ func _on_right_hand_hover_entered() -> void:
 	
 func _on_left_function_pointer_pointing_event(event: XRToolsPointerEvent) -> void:
 	if event.event_type == XRToolsPointerEvent.Type.ENTERED:
-		pointer_hover_rumbler.rumble_hand($"../LeftHand")
+		if event.target.is_in_group("DialogueChoice"):
+			pointer_hover_rumbler.rumble_hand($"../LeftHand")
 
 func _on_right_function_pointer_pointing_event(event: XRToolsPointerEvent) -> void:
 	if event.event_type == XRToolsPointerEvent.Type.ENTERED:
-		pointer_hover_rumbler.rumble_hand($"../RightHand")
+		if event.target.is_in_group("DialogueChoice"):
+			pointer_hover_rumbler.rumble_hand($"../RightHand")
