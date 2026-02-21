c282205aa32e4a4410296a2372e15660c0091954
AtomicAntt <anthonychi36@gmail.com>
2026-02-20 17:43:23 -0800

    Pressing UI buttons activates haptic feedback to the pressing controller

--- Player/haptics_manager.gd
+++ Player/haptics_manager.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var pickup_rumbler: XRToolsRumbler = $PickUpRumbler
 @onready var hover_rumbler: XRToolsRumbler = $HoverItemRumbler
 @onready var pointer_hover_rumbler: XRToolsRumbler = $PointerHoverRumbler
+@onready var pointer_pressed_rumbler: XRToolsRumbler = $PointerPressedRumbler
 
 func _on_left_hand_picked_up(_what: Variant) -> void:
 	pickup_rumbler.rumble_hand($"../LeftHand")
@@ -21,8 +22,14 @@ func _on_left_function_pointer_pointing_event(event: XRToolsPointerEvent) -> voi
 	if event.event_type == XRToolsPointerEvent.Type.ENTERED:
 		if event.target.is_in_group("DialogueChoice"):
 			pointer_hover_rumbler.rumble_hand($"../LeftHand")
+	elif event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		if event.target.is_in_group("DialogueChoice"):
+			pointer_pressed_rumbler.rumble_hand($"../LeftHand")
 
 func _on_right_function_pointer_pointing_event(event: XRToolsPointerEvent) -> void:
 	if event.event_type == XRToolsPointerEvent.Type.ENTERED:
 		if event.target.is_in_group("DialogueChoice"):
 			pointer_hover_rumbler.rumble_hand($"../RightHand")
+	elif event.event_type == XRToolsPointerEvent.Type.PRESSED:
+		if event.target.is_in_group("DialogueChoice"):
+			pointer_pressed_rumbler.rumble_hand($"../RightHand")
--- UI/Dialogue/Dialogue3D.gd
+++ UI/Dialogue/Dialogue3D.gd
@@ -1,6 +1,8 @@
 @tool
 extends XRToolsViewport2DIn3D
 
+## Generic node for dialogue UI elements like the text and buttons
+
 @onready var xr_camera: XRCamera3D = get_tree().get_nodes_in_group("XRCamera3D")[0]
 
 func _physics_process(_delta: float) -> void:
