3af09fb799da29fc5cd6b086f0c62ed9502c37fb
AtomicAntt <anthonychi36@gmail.com>
2026-02-23 19:20:33 -0800

    Add a system that allows a dialogue container to dynamically add options with given arguments.

--- UI/Dialogue/DialogueButton2D.gd
+++ UI/Dialogue/DialogueButton2D.gd
@@ -1,8 +1,20 @@
 class_name DialogueButton2D
 extends Node2D
 
-## If a scene_name is given, then pressing this dialogue button will switch scenes to the given name.
-var scene_name: String = "GrassLevel"
+@onready var text_label: Button = $Button
+var function_called: String
+var arguments_given: Array[String]
 
 func _on_button_pressed() -> void:
-	Global.switch_scene(scene_name)
+	#Global.switch_scene(scene_name)
+	print("button pressed")
+
+func set_text(new_text: String) -> void:
+	text_label.text = new_text
+
+func set_function(new_function: String) -> void:
+	function_called = new_function
+
+func set_arguments(new_arguments: Array[String]) -> void:
+	for i in range(new_arguments.size()):
+		arguments_given[i] = new_arguments[i]
--- UI/Dialogue/dialogue_container_3d.gd
+++ UI/Dialogue/dialogue_container_3d.gd
@@ -1,16 +1,30 @@
 class_name DialogueContainer
 extends Node3D
 
-var dialogue_text3D: PackedScene = preload("res://UI/Dialogue/DialogueText3D.tscn")
-var dialogue_text2D: PackedScene = preload("res://UI/Dialogue/DialogueText2D.tscn")
 var dialogue_choice3D: PackedScene = preload("res://UI/Dialogue/DialogueChoice3D.tscn")
-var dialogue_choice2D: PackedScene = preload("res://UI/Dialogue/DialogueChoice2D.tscn")
 
 @onready var dialogue_text: Dialogue3D = $DialogueText
 @onready var dialogue_text_2d: DialogueText2D = dialogue_text.get_scene_instance()
+@export var text_offset: float = 0.4 # offset between the text and the first option
+@export var choice_offset: float = 0.25
+
+
+func _ready() -> void:
+	add_option("Yo, teleport me", "", [])
+	add_option("JK", "", [])
 
 func set_text(new_text: String) -> void:
 	dialogue_text_2d.set_text(new_text)
 
 func get_text() -> String:
 	return dialogue_text_2d.get_text()
+
+func add_option(text: String, function_name: String, arguments_passed: Array[String]) -> void:
+	var dialogue_choice3D_instance: Dialogue3D = dialogue_choice3D.instantiate()
+	add_child(dialogue_choice3D_instance)
+	dialogue_choice3D_instance.global_position = dialogue_text.global_position - Vector3(0, ((get_child_count()-2)*choice_offset) + text_offset, 0)
+	
+	var dialogue_choice2D: DialogueButton2D = dialogue_choice3D_instance.get_scene_instance()
+	dialogue_choice2D.set_text(text)
+	dialogue_choice2D.set_function(function_name)
+	dialogue_choice2D.set_arguments(arguments_passed)
