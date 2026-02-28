95d4a1a96618dd30d54c2c8277bc03d44239d9a4
AtomicAntt <anthonychi36@gmail.com>
2026-02-28 00:18:41 -0800

    Add haptic feedback depending on linear and angular velocity.

--- GameObjects/Weapons/MeleeWeapon.gd
+++ GameObjects/Weapons/MeleeWeapon.gd
@@ -8,13 +8,13 @@ func _on_hitbox_body_entered(body: Node3D) -> void:
 			var xr_controller: XRController3D = _grab_driver.primary.controller
 			var xr_linear_velocity: Vector3 = xr_controller.get_pose().linear_velocity
 			var xr_angular_velocity: Vector3 = xr_controller.get_pose().angular_velocity
-			#print("Linear velocity on impact: " + str(xr_linear_velocity))
-			#print("Linear velocity magnitude: " + str(xr_linear_velocity.length()))
-			#print("Angular velocity on impact: " + str(xr_angular_velocity))
-			#print("Angular velocity magnitude: " + str(xr_angular_velocity.length()))
+			var xr_linear_magnitude: float = xr_linear_velocity.length()
+			var xr_angular_magnitude: float = xr_angular_velocity.length()
 			
-			get_node("DebugLabel").text = "lv: " + str(xr_linear_velocity.snapped(Vector3(0.1, 0.1, 0.1))) + "," + str(snapped(xr_linear_velocity.length(), 0.01))
-			get_node("DebugLabel2").text = "lv: " + str(xr_angular_velocity.snapped(Vector3(0.1, 0.1, 0.1))) + "," + str(snapped(xr_angular_velocity.length(), 0.01))
+			get_node("DebugLabel").text = "lv: " + str(xr_linear_velocity.snapped(Vector3(0.1, 0.1, 0.1))) + "," + str(snapped(xr_linear_magnitude, 0.01))
+			get_node("DebugLabel2").text = "av: " + str(xr_angular_velocity.snapped(Vector3(0.1, 0.1, 0.1))) + "," + str(snapped(xr_angular_magnitude, 0.01))
 			
-			
-			
+			if (xr_linear_magnitude >= 1.0) or (xr_angular_magnitude >= 1.0):
+				var rumbler: XRToolsRumbleEvent = $ImpactRumbler.event
+				rumbler.magnitude = clampf(((xr_angular_magnitude/2) + (xr_linear_magnitude/2))/25, 0.0, 1.0)
+				$ImpactRumbler.rumble_hand(_grab_driver.primary.controller)
