dab3432eddf7a2064c61eb813c9a2dec95f7c837
AtomicAntt <anthonychi36@gmail.com>
2026-03-04 11:59:14 -0800

    Remove items unused in-game from test level, remove debug linear/angular velocities/magnitude debug labels.

--- Player/player.gd
+++ Player/player.gd
@@ -4,3 +4,6 @@ extends XROrigin3D
 func _on_trigger_button_pressed(name: String) -> void:
 	if name == "trigger_click":
 		get_tree().call_group("DialogueText2D", "proceed_dialogue")
+	
+func _process(_delta: float) -> void:
+	$RightHand/FPSLabel.text = str(Engine.get_frames_per_second())
