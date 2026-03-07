f6ebf9134b84d1c4ce6ea1df825bd445b765b78b
AtomicAntt <anthonychi36@gmail.com>
2026-03-07 10:19:39 -0800

    Change to a cubic bezier curve for the pickup indicator.

--- Player/pickup_indicator.gd
+++ Player/pickup_indicator.gd
@@ -20,6 +20,7 @@ func start(new_destination_object: Node3D) -> void:
 ## Stops the pickup indicator.
 func stop() -> void:
 	generating_points = false
+	points = []
 
 ## Generate points given a global_position destination (p2)
 ## Likely used by FunctionPickup whenever a ranged grab is available.
@@ -38,7 +39,35 @@ func generate_points(p2: Vector3) -> void:
 		
 		points.append(r)
 
+## Generate points given a global_position destination (p3)
+## Likely used by FunctionPickup whenever a ranged grab is available.
+## This is the new way of doing it.
+func generate_points_cubic(p3: Vector3) -> void:
+	points = []
+	
+	var p0: Vector3 = global_position
+	
+	# This is the y direction of the hand faces * magnitude of 0.1
+	# Remember that -z is the forward direction.
+	
+	var offset: Vector3 = get_parent().transform.basis.y * 0.1
+	var p1: Vector3 = p0 + offset
+	# Midpoint with an offset of the forward direction of the hand.
+	var p2: Vector3 = ((p3 + p0)/2) + offset
+	for i in range(num_segments+1):
+		var t: float = float(i)/float(num_segments)
+		var q0: Vector3 = p0.lerp(p1, t)
+		var q1: Vector3 = p1.lerp(p2, t)
+		var q2: Vector3 = p2.lerp(p3, t)
+		
+		var r0: Vector3 = q0.lerp(q1, t)
+		var r1: Vector3 = q1.lerp(q2, t)
+		
+		var s = r0.lerp(r1, t)
+		
+		points.append(s)
+
 func _physics_process(_delta: float) -> void:
 	if generating_points and is_instance_valid(destination_object):
 		var destination_position = destination_object.global_position
-		generate_points(destination_position)
+		generate_points_cubic(destination_position)
