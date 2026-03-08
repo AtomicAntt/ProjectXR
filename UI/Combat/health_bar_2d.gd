5850cb7442f916f615c77c8c5f76223377144a59
AtomicAntt <anthonychi36@gmail.com>
2026-03-07 21:07:05 -0800

    Add tweening damage indicators to health bars.

--- UI/Combat/health_bar_2d.gd
+++ UI/Combat/health_bar_2d.gd
@@ -1,5 +1,15 @@
 class_name HealthBar2D
 extends Node2D
 
+var tween: Tween
+
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var damage_indicator: ProgressBar = $ProgressBar/DamageIndicator
+
 func set_value(health: float) -> void:
-	$ProgressBar.value = health
+	progress_bar.value = health
+	set_damage_indicator(health)
+	
+func set_damage_indicator(health: float) -> void:
+	tween = create_tween()
+	tween.tween_property(damage_indicator, "value", health, 0.5)
